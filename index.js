b4df4f16733bceac3175fffe6aaebcefee92424a
Nikhil Maurya <nikhil.maurya@nikhilmaurya-ss305.successivetech.com>
2023-11-27 13:11:51 +0530

    Assignment-1 Updated

--- index.js
+++ index.js
@@ -5,45 +5,51 @@ const args = process.argv;
 const operation = args[2];
 const num1 = parseFloat(args[3]);
 const num2 = parseFloat(args[4]);
-
 const sumNumbers = add(num1, num2);
 const subtractNumbers = sub(num1, num2);
 const multiplyNumbers = mul(num1, num2);
 const divideNumbers = div(num1, num2);
 let result = 0;
+let operationFeasible = true
 
-if (operation === "Addition") {
+if (operation === "+") {
     result += sumNumbers;
-} else if (operation === "Subtraction") {
+} else if (operation === "-") {
     result += subtractNumbers;
-} else if (operation === "Multiply") {
+} else if (operation === "*") {
     result += multiplyNumbers;
-} else {
+} else if (operation === "/") {
     result += divideNumbers;
 }
+else {
+    operationFeasible = false
+    console.log("Please perform mentioned operations only.")
+}
 
-if (fs.existsSync("OperationResultData.csv")) {
-    fs.appendFile(
-        "OperationResultData.csv",
-        `${operation},${num1},${num2},${result}\n`,
-        (err) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log("Result successfully appended to the file.");
+if (operationFeasible) {
+    if (fs.existsSync("OperationResultData.csv")) {
+        fs.appendFile(
+            "OperationResultData.csv",
+            `${operation},${num1},${num2},${result}\n`,
+            (err) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log("Result successfully appended to the file.");
+                }
             }
-        }
-    );
-} else {
-    fs.appendFile(
-        "OperationResultData.csv",
-        `Operation,Num1,Num2,Result\n${operation},${num1},${num2},${result}\n`,
-        (err) => {
-            if (err) {
-                console.error(err);
-            } else {
-                console.log("New file created successfully and result appended in that.");
+        );
+    } else {
+        fs.appendFile(
+            "OperationResultData.csv",
+            `Operation,Num1,Num2,Result\n${operation},${num1},${num2},${result}\n`,
+            (err) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log("New file created successfully and result appended in that.");
+                }
             }
-        }
-    );
+        );
+    }
 }
