02278ff97ce23530a619ef34e00c02def9f5f2f5
Nikhil Maurya <nikhil.maurya@nikhilmaurya-ss305.successivetech.com>
2023-12-10 19:30:52 +0530

    assignment-4

--- src/app.js
+++ src/app.js
@@ -6,10 +6,6 @@ app.get('/', (req, res) => {
     res.send('Welcome To Home Page.')
 })
 app.use('/routes', userRoutes)
-<<<<<<< HEAD
-=======
-
->>>>>>> 8b64085ddc7940bb94bc969a23f84a2e4bb21d94
 const port = 8000
 
 app.listen(port, () => {
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,10 +1,13 @@
 import express from 'express'
 import jwt from 'jsonwebtoken'
 import customMiddleware from "../middleware/customMiddleware.js"
+import authMiddleware from '../middleware/authMiddleware.js'
+import errorHandlerMiddleware from "../middleware/errorHandlingMiddleware.js"
 import limiter from '../middleware/limiterMiddleware.js'
 import { middleware1, middleware2 } from '../middleware/middlewareFunctions.js'
-import foodData from '../utils/dataseeding.js'
-import nameData from '../utils/mockData.js'
+import userData from '../utils/mockData.js'
+import customHeaderMiddleware from '../middleware/customheaderMiddleware.js'
+import { dataSeeder } from '../utils/dataseeding.js';
 import queryValidation from '../middleware/queryMiddleware.js'
 import validateRegistration from '../utils/registrationValidationSchema.js'
 import locationMiddleware from '../middleware/locationMiddleware.js'
@@ -21,34 +24,36 @@ router.use(errorHandlerMiddleware);
 router.post('/login', customMiddleware, (req, res) => {
   const { email, password } = req.body;
   const user = userData.find(u => u.email === email && u.password === password);
-
-  router.get('/login', (req, res) => {
-    const { name } = req.body
-    const user = nameData.find((user) => user.name === name)
-    if (user) {
-      const token = jwt.sign(
-        { email: user.email },
-        secretKey,
-        { expiresIn: '1h' }
-      );
-      res.json({ token });
-    } else {
-      res.status(401).json({ message: 'Invalid email or password' });
-    }
-  })
+  if (user) {
+    const token = jwt.sign(
+      { email: user.email },
+      secretKey,
+      { expiresIn: '1h' }
+    );
+    res.json({ token });
+  } else {
+    res.status(401).json({ message: 'Invalid email or password' });
+  }
+})
 
 
-  router.get('/chainMiddleware', customMiddleware, authMiddleware, middleware1, middleware2, (req, res) => {
-    res.send("Middleware Called")
-  })
+router.get('/chainMiddleware', customMiddleware, authMiddleware, middleware1, middleware2, (req, res) => {
+  res.send("Middleware Called")
+})
 
-  // http://localhost:8000/routes/query?value=ew (hit query route like this)
-  router.get('/query', queryValidation, (req, res) => {
-    res.json("Query Send")
-  })
+router.post('/seedData', customMiddleware, authMiddleware, (req, res) => {
+  const foodData = dataSeeder();
+  res.json(foodData);
+  console.log("Data seeding completed");
+});
 
-  router.get('/location', locationMiddleware, (req, res) => {
-    res.json({ message: 'Access granted!' });
-  });
+// http://localhost:8000/routes/query?value=ew (hit query route like this)
+router.get('/query', queryValidation, (req, res) => {
+  res.json("Query Send")
+})
 
-  export default router
+router.get('/location', locationMiddleware, (req, res) => {
+  res.json({ message: 'Access granted!' });
+});
+
+export default router
