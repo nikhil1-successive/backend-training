185b6bedcb7f2cd8e28d472abe7837a74e757a84
Nikhil Maurya <nikhil.maurya@nikhilmaurya-ss305.successivetech.com>
2023-12-29 23:56:43 +0530

    assignment-7 updated

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -28,7 +28,7 @@ router.use(express.json());
 router.use(rateLimiter.processRequest.bind(rateLimiter));
 router.use(errorHandler.processError.bind(errorHandler));
 
-// hit this route by passing email and password in body. Get email and body from mockData.ts file. 
+// hit this route by passing email and password in body. Get email and body from mockData.ts file.
 router.post('/login', (req: Request, res: Response) => {
   const { email, password }: { email: string; password: string } = req.body;
   const user: IUser | undefined = UserData.getUserByEmail(email);
@@ -56,18 +56,18 @@ router.post('/seeddata', (req: Request, res: Response) => {
   console.log('Data seeding completed');
 });
 
-// hit this route as (http://localhost:8000/routes/signup) with body {"email":"nikhil@successive.com", "username":"aaa", "password":"nik123"}
-router.post("/signup", validateRequest("login"), validateRegistration, (req: Request, res: Response) => {
+// hit this route as (http://localhost:8000/routes/signup) with body {'email':'nikhil@successive.com', 'username':'aaa', 'password':'nik123'}
+router.post('/signup', validateRequest('login'), validateRegistration, (req: Request, res: Response) => {
   res.json({ success: true });
 });
 
-//hit this route ex-(http://localhost:8000/routes/query?value=2)
+// hit this route ex-(http://localhost:8000/routes/query?value=2)
 router.get('/query', queryMiddleware.processRequest, (req: Request, res: Response) => {
   res.json('Query Send');
 });
 
-router.get("/location", geoLocationMiddleware.middleware, (req: Request, res: Response) => {
-  res.send("You are authorized!");
+router.get('/location', geoLocationMiddleware.middleware, (req: Request, res: Response) => {
+  res.send('You are authorized!');
 });
 
 router.get('/customheader', myCustomHeaderMiddleware, (req: Request, res: Response) => {
@@ -78,7 +78,7 @@ router.get('/custommiddleware', customMiddleware.middleware, (req: Request, res:
   console.log('Route logic executed');
 });
 
-// hit this route as http://localhost:8000/routes/params with body { "arg1":"12","arg2":"22"}
+// hit this route as http://localhost:8000/routes/params with body { 'arg1':'12','arg2':'22'}
 router.post('/params', validateParametersMiddleware.validateParameters, (req: Request, res: Response) => {
   res.json({
     message: 'Success',
