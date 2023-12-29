337b3609b0037871a9244a36de7c85fddfb57145
nikhil1-successive <146409966+nikhil1-successive@users.noreply.github.com>
2023-12-29 17:20:05 +0530

    Update userRoutes.ts

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import express, { Request, Response, Router } from 'express';
 import jwt from 'jsonwebtoken';
 import UserData, { IUser } from '../utils/MockData';
